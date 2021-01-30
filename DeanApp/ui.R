6f68130ff079fd9163644b8e0b6d7f3ea7e997af
Jeremy Dean <sdean.jeremy@gmail.com>
2021-01-30 13:26:14 -0800

    changed input ranges and added outputs

--- DeanApp/ui.R
+++ DeanApp/ui.R
@@ -13,20 +13,20 @@ shinyUI(fluidPage(
                           "Random Forest (More Accurate but slow)" = "rf")),
             selectInput("cyls", "Number of Cylinders", c(4, 6, 8)),
             sliderInput("displacement", "Displayment (cubic inches)", 
-                        min(mtcars$disp) - 50, max(mtcars$disp) + 50, 
+                        min(mtcars$disp) - 25, max(mtcars$disp) + 25, 
                         mean(mtcars$disp)),
             sliderInput("horse", "Horsepower",
-                        min(mtcars$hp) - 10, max(mtcars$hp) + 200,
+                        min(mtcars$hp) - 10, max(mtcars$hp) + 50,
                         mean(mtcars$hp)),
             sliderInput("ratio", "Rear Axle Ratio",
-                        min(mtcars$drat) - 1, max(mtcars$drat) + 1,
+                        min(mtcars$drat) - .5, max(mtcars$drat) + .5,
                         mean(mtcars$drat)),
             sliderInput("weight", "Weight (lbs)",
-                        (min(mtcars$wt) * 1000) - 1000, 
-                        (max(mtcars$wt) * 1000) + 1000,
+                        (min(mtcars$wt) * 1000) - 200, 
+                        (max(mtcars$wt) * 1000) + 200,
                         mean(mtcars$wt) * 1000),
             sliderInput("quart", "Quarter Mile Time",
-                        min(mtcars$qsec) - 2, max(mtcars$qsec) + 2,
+                        min(mtcars$qsec) - .25, max(mtcars$qsec) + .25  ,
                         mean(mtcars$qsec)),
             sliderInput("carbs", "Number of Carburetors", 1 , 8, 4),
             selectInput("trans", "Transmission Type", 
@@ -34,11 +34,14 @@ shinyUI(fluidPage(
             selectInput("eng", "Engine Shape",
                         c("V-shaped" = 0, "Straight" = 1)),
             numericInput("gears", "Number of Gears", 4, 3, 5),
-            actionButton("done", "Submit"),            
+            actionButton("done", "Submit")            
         ),
 
         mainPanel(
-            p("Lets see how many miles your car will get per gallon.")
+            p("Lets see how many miles your car will get per gallon."),
+            textOutput("miles"),
+            p("Here's how your car compares to others.")
+            plotOutput("hist")
         )
     )
 ))
